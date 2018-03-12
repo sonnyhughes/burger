02fb8f08554c5a168617679579a8fb71b046ddd7
Sonny <richardhughesjr@gmail.com>
2018-03-11 20:27:31 -0500

    Adds database schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name) VALUES ('Gourdon Hamsey');
+INSERT INTO burgers(burger_name) VALUES ("Don't You Four Cheddar 'Bout Me'");
+INSERT INTO burgers(burger_name) VALUES ('Hit Me With Your Best Shallot');
+INSERT INTO burgers(burger_name) VALUES ('Olive and Let Die');
+INSERT INTO burgers(burger_name) VALUES ('Bohemian Radishy');
+INSERT INTO burgers(burger_name) VALUES ('Baby You Can Chive My Car');
+INSERT INTO burgers(burger_name) VALUES ('Sweet Home Avocado');
+INSERT INTO burgers(burger_name) VALUES ('Mission A-Corn-Plished');
+INSERT INTO burgers(burger_name) VALUES ('If Looks Could Kale');
+INSERT INTO burgers(burger_name) VALUES ("Eggers Can't Be Cheesers");
+INSERT INTO burgers(burger_name) VALUES ('Peas and Thank You');
+INSERT INTO burgers(burger_name) VALUES ('Beet-er Late Than Never');
+
