c3b1cb746142271e5482d1c02146affa6f6229c1
Sonny <richardhughesjr@gmail.com>
2018-03-12 22:24:23 -0500

    adds JAWSBD to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,6 @@
 var mysql = require('mysql');
 
-//LOCAL DATABASE CONNECTION
-// var connection = mysql.createConnection({
-//     port: 3306,
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'burgers_db'
-// });
-
-//DEPLOYED DATABASE CONNECTION
+// DATABASE CONNECTION
 if (process.env.JAWSDB_URL) {
     var connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
